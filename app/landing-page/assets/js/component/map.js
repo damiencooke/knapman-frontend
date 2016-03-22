bdd8fc5a383cb33d340ecb6b13b2d24ae370053a
damiencooke <damien.cooke@digital.hmrc.gov.uk>
2016-03-22 17:48:26 +0000

    so much changes

--- app/landing-page/assets/js/component/map.js
+++ app/landing-page/assets/js/component/map.js
@@ -3,8 +3,8 @@ $(function(){
         "use strict";
         
         // set your google maps parameters
-        var $latitude = -6.193948, //If you unable to find latitude and longitude of your address. Please visit http://www.latlong.net/convert-address-to-lat-long.html you can easily generate.
-            $longitude = 106.849184,
+        var $latitude = 50.79124, //If you unable to find latitude and longitude of your address. Please visit http://www.latlong.net/convert-address-to-lat-long.html you can easily generate.
+            $longitude = 0.30779,
             $map_zoom = 16; /* ZOOM SETTING */
 
         // google map custom marker icon
@@ -35,7 +35,6 @@ $(function(){
             scrollwheel: false,
             styles: style
         };
-        
         // inizialize the map
         var map = new google.maps.Map(document.getElementById('google-container'), map_options);
     
@@ -46,5 +45,31 @@ $(function(){
             visible: true,
             icon: $marker_url
         });
+
+
+// trying to do the radius couldn't get it to work will fix later
+//        var citymap = {
+//          Eastbourne: {
+//            center: {lat: 50.79124, lng: 0.30779},
+//          },
+//        };
+//
+//          // Construct the circle for each value in citymap.
+//          // Note: We scale the area of the circle based on the population.
+//          for (var city in citymap) {
+//            // Add the circle for this city to the map.
+//            var cityCircle = new google.maps.Circle({
+//              strokeColor: '#FF0000',
+//              strokeOpacity: 0.8,
+//              strokeWeight: 2,
+//              fillColor: '#FF0000',
+//              fillOpacity: 0.35,
+//              map: map,
+//              center: citymap[city].center,
+//              radius: 25
+//            });
+//          }
+//        }
+
     });
 });
